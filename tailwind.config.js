987b905039832fdc8d823ad3d374fab852fbded1
tokky01 <war.verlinde@gmail.com>
2023-06-21 17:03:53 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
+
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            fontFamily: {
+                'barlow': ['Barlow', 'sans-serif'],
+
+            }
+        },
+        colors: {
+            'josblue': '#3297BF',
+            'joslightblue': '#B8E1F1',
+            'josyellow': '#F1B71C',
+            'joslightorange': '#F0B077',
+            'josorange': '#F15E22',
+            'white': '#FFFFFF',
+        }
     },
-  },
-  plugins: [],
+    plugins: [],
 }
